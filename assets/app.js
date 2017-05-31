b2c2c2165586ee28e2a30a633c5956e8e9447998
Jonathan Heslop <jonathan.heslop@digitaslbi.com>
2017-05-31 17:30:36 +0100

    product page updates

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
   $('.home-slider').slick({
-      arrows: false,
-      autoplay: true,
-      autoplaySpeed: 7000,
-      dots: true
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    dots: true
+  });
+
+  $('.play-button').click(function(e){
+    e.preventDefault();
+    this.remove();
+    $('.video-container img').remove();
   });
 });
